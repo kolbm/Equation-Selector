280e70c8879c5093f572984aeca19f6b291ffc4e
Marc Kolb <kolbm@dunellenschools.org>
2024-10-31 09:58:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import sympy as sp
 
 # Define symbols for equations
-x, x0, v0, v, a, t = sp.symbols('x x0 v0 v a t')
+x, v0, v, a, t = sp.symbols('x v0 v a t')
 
-# Define kinematic equations
+# Define kinematic equations with x0 set to 0
 eq1 = sp.Eq(v, v0 + a * t)  # v = v0 + a * t
-eq2 = sp.Eq(x, x0 + v0 * t + 0.5 * a * t**2)  # x = x0 + v0 * t + 0.5 * a * t^2
-eq3 = sp.Eq(v**2, v0**2 + 2 * a * (x - x0))  # v^2 = v0^2 + 2a * Δx
+eq2 = sp.Eq(x, v0 * t + 0.5 * a * t**2)  # x = v0 * t + 0.5 * a * t^2
+eq3 = sp.Eq(v**2, v0**2 + 2 * a * x)  # v^2 = v0^2 + 2a * x
 
 # Display title and logo
 st.image("https://github.com/kolbm/Equation-Selector/blob/main/eqlogo.jpg?raw=true")
@@ -41,30 +41,30 @@ if st.button("Show Suggested Equation"):
         if knowns['v0'] and knowns['t'] and knowns['a']:
             st.latex(sp.latex(eq2))
         elif knowns['v0'] and knowns['v'] and knowns['a']:
-            st.latex(sp.latex(eq3))
+            st.latex(sp.latex(sp.solve(eq3, x)[0]))
     elif solving_for == 'Initial velocity (v0)':
         if knowns['v'] and knowns['t'] and knowns['a']:
             st.latex(sp.latex(eq1))
         elif knowns['v'] and knowns['x'] and knowns['a']:
-            st.latex(sp.latex(eq3))
+            st.latex(sp.latex(sp.solve(eq3, v0)[0]))
     elif solving_for == 'Final velocity (v)':
         if knowns['v0'] and knowns['t'] and knowns['a']:
             st.latex(sp.latex(eq1))
         elif knowns['v0'] and knowns['x'] and knowns['a']:
-            st.latex(sp.latex(eq3))
+            st.latex(sp.latex(sp.solve(eq3, v)[0]))
     elif solving_for == 'Acceleration (a)':
         if knowns['v0'] and knowns['t'] and knowns['v']:
             st.latex(sp.latex(eq1))
         elif knowns['v0'] and knowns['x'] and knowns['v']:
-            st.latex(sp.latex(eq3))
+            st.latex(sp.latex(sp.solve(eq3, a)[0]))
     elif solving_for == 'Time (t)':
         if knowns['v0'] and knowns['a'] and knowns['v']:
             st.latex(sp.latex(eq1))
         elif knowns['v0'] and knowns['x'] and knowns['a']:
-            st.latex(sp.latex(eq2))
+            st.latex(sp.latex(sp.solve(eq2, t)))
 
     # Provide info if no equation is displayed due to missing inputs
-    if not (knowns['v0'] or knowns['v'] or knowns['a'] or knowns['t'] or knowns['x']):
+    if not any(knowns.values()):
         st.warning("Please select at least two known values to suggest an equation.")
 else:
     st.info("Click the button to show the suggested equation based on your inputs.")
