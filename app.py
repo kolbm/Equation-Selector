83a79b03e98685bf52dec745631dcaf76981559d
Marc Kolb <kolbm@dunellenschools.org>
2024-10-31 10:01:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -33,35 +33,39 @@ knowns = {
 st.subheader("What are you solving for?")
 solving_for = st.selectbox("Choose the unknown:", ["Displacement (x)", "Initial velocity (v0)", "Final velocity (v)", "Acceleration (a)", "Time (t)"])
 
+# Function to display large equations
+def display_large_equation(equation):
+    st.markdown(f"<h2 style='text-align: center;'>{sp.latex(equation)}</h2>", unsafe_allow_html=True)
+
 # Button to trigger the equation selection
 if st.button("Show Suggested Equation"):
     # Determine which equation arrangement to suggest based on known values
     st.subheader("Suggested Equation:")
     if solving_for == 'Displacement (x)':
         if knowns['v0'] and knowns['t'] and knowns['a']:
-            st.latex(sp.latex(eq2))
+            display_large_equation(eq2)
         elif knowns['v0'] and knowns['v'] and knowns['a']:
-            st.latex(sp.latex(sp.solve(eq3, x)[0]))
+            display_large_equation(sp.solve(eq3, x)[0])
     elif solving_for == 'Initial velocity (v0)':
         if knowns['v'] and knowns['t'] and knowns['a']:
-            st.latex(sp.latex(eq1))
+            display_large_equation(eq1)
         elif knowns['v'] and knowns['x'] and knowns['a']:
-            st.latex(sp.latex(sp.solve(eq3, v0)[0]))
+            display_large_equation(sp.solve(eq3, v0)[0])
     elif solving_for == 'Final velocity (v)':
         if knowns['v0'] and knowns['t'] and knowns['a']:
-            st.latex(sp.latex(eq1))
+            display_large_equation(eq1)
         elif knowns['v0'] and knowns['x'] and knowns['a']:
-            st.latex(sp.latex(sp.solve(eq3, v)[0]))
+            display_large_equation(sp.solve(eq3, v)[0])
     elif solving_for == 'Acceleration (a)':
         if knowns['v0'] and knowns['t'] and knowns['v']:
-            st.latex(sp.latex(eq1))
+            display_large_equation(eq1)
         elif knowns['v0'] and knowns['x'] and knowns['v']:
-            st.latex(sp.latex(sp.solve(eq3, a)[0]))
+            display_large_equation(sp.solve(eq3, a)[0])
     elif solving_for == 'Time (t)':
         if knowns['v0'] and knowns['a'] and knowns['v']:
-            st.latex(sp.latex(eq1))
+            display_large_equation(eq1)
         elif knowns['v0'] and knowns['x'] and knowns['a']:
-            st.latex(sp.latex(sp.solve(eq2, t)))
+            display_large_equation(sp.solve(eq2, t))
 
     # Provide info if no equation is displayed due to missing inputs
     if not any(knowns.values()):
